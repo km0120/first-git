2ce0834a0f2ff324b1b158f520219c609d6cfc2f
km0120 <19860120km@gmail.com>
2019-04-22 21:06:59 +0900

    2019/04/22 21:06

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -34,65 +34,6 @@ public class UpdateServlet extends HttpServlet {
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
-/*    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String _token = (String)request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
-            EntityManager em = DBUtil.createEntityManager();
-
-            // セッションスコープからメッセージのIDを取得して
-            // 該当のIDのメッセージ1件のみをデータベースから取得
-            Tasks m = em.find(Tasks.class, (Integer)(request.getSession().getAttribute("task_id")));
-
-            // フォームの内容を各フィールドに上書き
-            String title = request.getParameter("title");
-            m.setTitle(title);
-
-            String content = request.getParameter("content");
-            m.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
-
-            // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
-            List<String> errors = TaskValidator.validate(m);
-            if(errors.size() > 0) {
-                em.close();
-
-                // フォームに初期値を設定、さらにエラーメッセージを送る
-                request.setAttribute("_token", request.getSession().getId());
-                request.setAttribute("tasks", m);
-                request.setAttribute("errors", errors);
-
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/edit.jsp");
-                rd.forward(request, response);
-            } else {
-                // データベースを更新
-                em.getTransaction().begin();
-                em.getTransaction().commit();
-                request.getSession().setAttribute("flush", "更新が完了しました。");
-                em.close();
-
-                // セッションスコープ上の不要になったデータを削除
-                request.getSession().removeAttribute("task_id");
-
-                // indexページへリダイレクト
-                response.sendRedirect(request.getContextPath() + "/index");
-            }
-
-            // データベースを更新
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
-
-            // セッションスコープ上の不要になったデータを削除
-            request.getSession().removeAttribute("task_id");
-
-            // indexページへリダイレクト
-            response.sendRedirect(request.getContextPath() + "/index");
-        }
-    }*/
-
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = (String)request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
